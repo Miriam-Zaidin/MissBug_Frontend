2252c272dc8be7c79a28b659c79b602d44f03308
Miriam-Zaidin <mirizaidin@gmail.com>
2024-02-14 15:41:19 +0200

    CRUDL end 2 end - phase 2

--- src/RootCmp.jsx
+++ src/RootCmp.jsx
@@ -5,7 +5,8 @@ import { Home } from './pages/Home.jsx'
 import { BugIndex } from './pages/BugIndex.jsx'
 import { BugDetails } from './pages/BugDetails.jsx'
 import { AboutUs } from './pages/AboutUs.jsx'
-import { Route, HashRouter as Router, Routes } from 'react-router-dom'
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
+import { UserIndex } from './pages/UserIndex.jsx'
 
 export function App() {
   return (
@@ -16,6 +17,7 @@ export function App() {
           <Routes>
             <Route path='/' element={<Home />} />
             <Route path='/bug' element={<BugIndex />} />
+            <Route path='/user' element={<UserIndex />} />
             <Route path='/bug/:bugId' element={<BugDetails />} />
             <Route path='/about' element={<AboutUs />} />
           </Routes>
